ee4ca99b21905cc7e8c8b4da0a1d748ee7e801c9
Ethan Chou <101858583+echoumcp1@users.noreply.github.com>
2023-11-20 20:47:42 -0500

    Update no_abs_refine_list.hs

--- no_abs_refine_list.hs
+++ no_abs_refine_list.hs
@@ -1,14 +1,1 @@
-module No_abs_refine_list where
 
-{-@ LIQUID "--exact-data-cons" @-}
-{-@ reflect sorted @-}
-{-@ sorted :: Ord a => [a] -> Bool @-}
-sorted [] = True
-sorted [x] = True
-sorted (x:y:xs) = x <= y && sorted (y:xs)
-
-type SList a = [a]
-{-@ type SList a =  {v:[a] | sorted v} @-}
-
-{-@ xs :: {v:SList Integer | len v = 1} @-}
-xs = [1]
